0247ca28042fb17787c78dd97c36aea203b9bad9
Muhammad Charaf <muhammad.charaf@gmail.com>
2020-12-28 20:01:01 +0200

    Adding feed ms

---
+++ api/udagram-feed-api/src/sequelize.ts
@@ -0,0 +1,11 @@
+import { Sequelize, SequelizeOptions } from 'sequelize-typescript';
+import { config } from './config/config';
+
+export const sequelize: Sequelize = new Sequelize({
+  'username': config.username,
+  'password': config.password,
+  'database': config.database,
+  'host': config.host,
+  'dialect': config.dialect,
+  'storage': ':memory:',
+} as SequelizeOptions);
