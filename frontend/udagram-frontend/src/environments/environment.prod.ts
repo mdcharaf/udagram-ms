eace4c564f8d6e4272f3c8f32658567c2a039a0d
Muhammad Charaf <muhammad.charaf@gmail.com>
2021-01-13 19:39:48 +0200

    Production build for frontend app docker file

--- frontend/udagram-frontend/src/environments/environment.prod.ts
+++ frontend/udagram-frontend/src/environments/environment.prod.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  usersApiHost: 'http://localhost:8080/api/v0',
-  feedApiHost: 'http://localhost:8081/api/v0',
+  usersApiHost: 'http://10.100.170.98:80/api/v0',
+  feedApiHost: 'http://10.100.73.120:8080/api/v0',
 };
 
 /*
